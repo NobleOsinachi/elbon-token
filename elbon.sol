7d75702d5dc29c86c4edc9b80873b7c6447d2fdc
Noble Osinachi <nobleosinachi98@gmail.com>
2023-07-27 19:54:50 +0100

    Update elbon.sol

--- elbon.sol
+++ elbon.sol
@@ -1,1 +1,12 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ElbonToken is ERC20 {
+    constructor() ERC20("Elbon Token", "ELB") {
+        uint256 initialSupply = 1000000 * 10**decimals();
+        _mint(msg.sender, initialSupply);
+    }
+}
