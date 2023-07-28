0b24943453429b1f78c94fd6b2b67ad55b1592cf
nobleosinachi <nobleosinachi98@gmail.com>
2023-07-28 11:28:29 +0100

    Deploy to Goerli Testnet

--- ElbonToken.sol
+++ ElbonToken.sol
@@ -1,12 +1,18 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ElbonToken is ERC20 {
-    constructor() ERC20("Elbon Token", "ELB") {
-        uint256 initialSupply = 1000000 * 10**decimals();
+    /**
+     * @dev Constructor to initialize the ERC20 token.
+     * @param _name The name of the token.
+     * @param _symbol The symbol (ticker) of the token.
+     */
+    constructor(string memory _name, string memory _symbol)
+        ERC20(_name, _symbol)
+    {
+        uint256 initialSupply = 1000 * 10**decimals();
         _mint(msg.sender, initialSupply);
     }
 }
